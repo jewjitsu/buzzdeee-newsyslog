38926afe61578e577845b683a304aec61210fd0d
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2014-11-17 15:24:55 +0100

    fix the regex, and some sanity checking

--- lib/puppet/provider/newsyslog/newsyslog.rb
+++ lib/puppet/provider/newsyslog/newsyslog.rb
@@ -18,7 +18,7 @@ Puppet::Type.type(:newsyslog).provide(
 	record_line :parsed,
 		# TODO: usergroup parsing
 		:fields => %w{name usergroup mode keep size when flags remainder},
-		:match  => %r{^\s*(/\S+)\s+(\w*:\w*|)\s+(\d\d\d)\s+(\d+)\s+(\d+|\*)\s+(\S+)\s+([BFMZ]*)\s*(.*)},
+		:match  => %r{^\s*(/\S+)\s+(\w*:\w*|)\s+(\d\d\d)\s+(\d+)\s+(\d+|\*)\s+(\S+)\s*([BFMZ]*)\s*(.*)},
 		:optional => %w{usergroup flags remainder},
 		:post_parse => proc { |hash|
 			if hash[:usergroup] == :absent
@@ -50,15 +50,25 @@ Puppet::Type.type(:newsyslog).provide(
 				hash[:group] = '' if hash[:group] == :absent
 				hash[:usergroup] = "#{hash[:owner]}:#{hash[:group]}"
 			end
-			if (hash.has_key? :command and (hash.has_key? :pidfile or hash.has_key? sigtype))
+			hash[:remainder] = '' if (hash[:remainder] == :absent or hash[:remainder] == nil)
+			if (hash.has_key? :monitor)
+				if (hash.has_key? :flags and hash[:flags].match(/M/))
+					hash[:remainder] = "#{hash[:monitor]}"
+				else
+					raise Puppet::ParseError, "Monitor mail receiver must be specified in combination with M flag in flags field."
+				end
+			end
+			if (hash.has_key? :command and (hash.has_key? :pidfile or hash.has_key? :sigtype))
 				raise Puppet::ParseError, "Given command parameter and pidfile or sigtype. Parameter command is mutually exclusive to the others."
 			end
-			if (hash.has_key? :pidfile and hash.has_key? :sigtype)
+			if ((hash.has_key? :pidfile and not hash.has_key? :sigtype) or (hash.has_key? :sigtype and not hash.has_key? :pidfile))
 				raise Puppet::ParseError, "Parameters pidfile and sigtype must be specified together."
 			end
 			if (hash.has_key? :command)
-				hash[:remainder] = "\"#{hash[:command]}\""
+				hash[:remainder] << " \"#{hash[:command]}\""
 			end
-
+			if (hash.has_key? :pidfile)
+				hash[:remainder] << " #{hash[:pidfile]} #{hash[:sigtype]}"
+			end
 		}
 end
